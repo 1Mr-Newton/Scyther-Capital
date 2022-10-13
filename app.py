59650a37e13af6dfc441296fd2f70f4e83d49044
1mr-newton <eaidoo015@gmail.com>
2022-10-13 15:43:36 +0000

    initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST', 'GET'])
 def homepage():
-  return render_template('home.html')
+  return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
